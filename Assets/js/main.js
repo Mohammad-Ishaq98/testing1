418036927c71f1726f66fa4051f7d22a37ff0eed
Mohammad-Ishaq98 <mdishaqibrohim@gmail.com>
2021-06-12 23:02:32 +0600

    first commit39

---
+++ Assets/js/main.js
@@ -0,0 +1,17 @@
+// (function ($){
+// 	"use strict";
+// 	jQuery(document).ready(function($){
+// 		$(".homepages-slides").owlCarousel({
+
+// 			items:1,
+// 			nav:true,
+// 			dosts:true,
+// 			loop:true,
+// 			autoplay:true,
+// 		});
+// 	});
+
+// 	  // jQuery(window).load(function(){
+
+// 	  // });
+// }(jQuery));
